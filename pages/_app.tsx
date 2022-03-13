6f8b5ba6ba358884ede186a2391be05febfeb381
luke beach <55807998+lamb572@users.noreply.github.com>
2022-03-13 21:42:22 +0000

    update mui

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,13 +1,35 @@
+import { CacheProvider, EmotionCache } from "@emotion/react"
+import CssBaseline from "@mui/material/CssBaseline"
+import { ThemeProvider } from "@mui/material/styles"
 import type { AppProps } from "next/app"
+import Head from "next/head"
 import { Provider as ReduxProvider } from "react-redux"
 import { store } from "../redux/store"
+import createEmotionCache from "../src/createEmotionCache"
 import "../styles/globals.css"
+import theme from "../styles/theme"
 
-function MyApp({ Component, pageProps }: AppProps) {
+// Client-side cache, shared for the whole session of the user in the browser.
+const clientSideEmotionCache = createEmotionCache()
+
+interface MyAppProps extends AppProps {
+  emotionCache?: EmotionCache
+}
+
+function MyApp(props: MyAppProps) {
+  const { Component, emotionCache = clientSideEmotionCache, pageProps } = props
   return (
-    <ReduxProvider store={store}>
-      <Component {...pageProps} />
-    </ReduxProvider>
+    <CacheProvider value={emotionCache}>
+      <Head>
+        <meta name="viewport" content="initial-scale=1, width=device-width" />
+      </Head>
+      <ThemeProvider theme={theme}>
+        <CssBaseline />
+        <ReduxProvider store={store}>
+          <Component {...pageProps} />
+        </ReduxProvider>
+      </ThemeProvider>
+    </CacheProvider>
   )
 }
 
