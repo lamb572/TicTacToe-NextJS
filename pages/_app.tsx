260ff5895cf4a2830070ab589ec820529b3fc917
Luke <lukebeach90@gmail.com>
2022-05-09 15:11:15 +0100

    removing mui and adding jest

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,36 +1,12 @@
-import { CacheProvider, EmotionCache } from "@emotion/react"
-import CssBaseline from "@mui/material/CssBaseline"
-import { ThemeProvider } from "@mui/material/styles"
 import type { AppProps } from "next/app"
-import Head from "next/head"
 import { Provider as ReduxProvider } from "react-redux"
 import { store } from "../redux/store"
-import createEmotionCache from "../src/createEmotionCache"
 import "../styles/globals.css"
-import theme from "../styles/theme"
-
-// Client-side cache, shared for the whole session of the user in the browser.
-const clientSideEmotionCache = createEmotionCache()
-
-interface MyAppProps extends AppProps {
-  emotionCache?: EmotionCache
-}
-
-function MyApp(props: MyAppProps) {
-  const { Component, emotionCache = clientSideEmotionCache, pageProps } = props
+function MyApp({ Component, pageProps }: AppProps) {
   return (
-    <CacheProvider value={emotionCache}>
-      <Head>
-        <meta name="viewport" content="initial-scale=1, width=device-width" />
-      </Head>
-      <ThemeProvider theme={theme}>
-        <CssBaseline />
-        <ReduxProvider store={store}>
-          <Component {...pageProps} />
-        </ReduxProvider>
-      </ThemeProvider>
-    </CacheProvider>
+    <ReduxProvider store={store}>
+      <Component {...pageProps} />
+    </ReduxProvider>
   )
 }
-
 export default MyApp
